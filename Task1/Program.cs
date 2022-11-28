85cbc7d2a423682b80d4892765084a05dfb256a3
Anastasia Sventsitskaya <anastasiasventsitskaya@MacBook-Air-Anastasia.local>
2022-11-29 03:04:52 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Write the numbers, when you're done write q: ");
+string input = string.Empty;
+int count = 0;
+while(true)
+{
+    input = Console.ReadLine();
+    if (input == "q")
+    {
+        break;
+    }
+    int number = Convert.ToInt32(input);
+    if (number > 0)
+    {
+        count +=1;
+    }
+} 
+
+
+Console.WriteLine("Колличество чисел больше нуля: "+count);
+
